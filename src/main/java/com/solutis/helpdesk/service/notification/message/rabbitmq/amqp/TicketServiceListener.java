1a6cdbf355c5f8d85c2f1df9068e27f3c1939902
caioCarmoAtSolutis <caio.carmo@solutis.com.br>
2026-09-20 20:44:27 -0300

    feat: add environment variables to queues routing key in rabbit listener class

--- src/main/java/com/solutis/helpdesk/service/notification/message/rabbitmq/amqp/TicketServiceListener.java
+++ src/main/java/com/solutis/helpdesk/service/notification/message/rabbitmq/amqp/TicketServiceListener.java
@@ -2,31 +2,22 @@ package com.solutis.helpdesk.service.notification.message.rabbitmq.amqp;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TicketServiceListener {
-    //@Value("${rabbitmq.ticket.created.routing-key}")
-    //private String ticketCreatedRoutingkey;
 
-    //@Value("${rabbitmq.ticket.assigned.routing-key}")
-    //private String ticketAssignedRoutingkey;
-
-    //@Value("${rabbitmq.ticket.status-changed.routing-key}")
-    //private String ticketStatusChangedRoutingkey;
-
-    @RabbitListener(queues = "ticket.created")
+    @RabbitListener(queues = "${rabbitmq.ticket.created.routing-key}")
     public void ListenQueueTicketCreated(Message message) {
         System.out.println("[TicketServiceListener] Received a new ticket created message" + message);
     }
 
-    @RabbitListener(queues = "ticket.assigned")
+    @RabbitListener(queues = "${rabbitmq.ticket.assigned.routing-key}")
     public void ListenQueueTicketAssigned(Message message) {
         System.out.println("[TicketServiceListener] Received a new ticket assigned message" + message);
     }
 
-    @RabbitListener(queues = "ticket.status-changed")
+    @RabbitListener(queues = "${rabbitmq.ticket.status-changed.routing-key}")
     public void ListenQueueTicketStatusChanged(Message message) {
         System.out.println("[TicketServiceListener] Received a new ticket status changed message" + message);
     }
